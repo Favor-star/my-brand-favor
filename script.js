42cb3995f1fb03be08aa811a39a2c5b77bd152b5
favor-star <favoureliab@gmail.com>
2024-02-14 17:35:52 +0200

    Fixed navbar in the mobile devices and fixed the icon of dark mode

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ Themeswitcher.forEach((elem) => {
     if (body.classList.contains("dark__mode")) {
       body.classList.remove("dark__mode");
       localStorage.setItem("colorMode", "white");
-      elem.classList.remove("bxs-moon");
-      elem.classList.add("bxs-sun");
+      elem.classList.remove("bxs-sun");
+      elem.classList.add("bxs-moon");
     } else {
       body.classList.add("dark__mode");
       localStorage.setItem("colorMode", "dark");
-      elem.classList.remove("bxs-sun");
-      elem.classList.add("bxs-moon");
+      elem.classList.remove("bxs-moon");
+      elem.classList.add("bxs-sun");
     }
   });
 });
@@ -26,3 +26,28 @@ const mode = (value) => {
   }
 };
 onload = mode(localStorage.getItem("colorMode"));
+//HANDLE THE NAVIGATION PANE
+const navigationPane = document.querySelector("#navigation__pane");
+document.getElementById("menu-bar").addEventListener("click", () => {
+  navigationPane.style.display = "block";
+  setTimeout(() => {
+    navigationPane.style.transform = "translateY(0)";
+  }, 50);
+});
+const handleMenuClose = () => {
+  navigationPane.style.transform = "translateY(-100%)";
+  setTimeout(() => {
+    navigationPane.style.display = "block";
+  }, 50);
+};
+document.getElementById("close").addEventListener("click", handleMenuClose);
+document
+  .querySelectorAll(".nav__sub")
+  .forEach((nav) => nav.addEventListener("click", handleMenuClose));
+
+window.onresize = () => {
+  if (window.innerWidth >= 834) {
+    navigationPane.style.display = "none";
+    console.log(window);
+  }
+};
