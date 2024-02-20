a0a8fa976c98f297695aa0b84bd5137eb33f218f
favor-star <favoureliab@gmail.com>
2024-02-20 14:36:36 +0200

    Handled the deleteUser in accounts management

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -17,20 +17,61 @@ logout.onclick = () => {
 };
 
 const accountsList = document.getElementById("list__of__accounts");
-function handleRegisteredUser() {
-  const users = JSON.parse(localStorage.getItem("users"));
-  console.log(users);
-  users.forEach((element) => {
+function handleRegisteredUser(users) {
+  users.forEach((element, index) => {
     const oneAccount = document.createElement("div");
     oneAccount.classList.add("one__account");
+    oneAccount.setAttribute("data-target", index);
+
     oneAccount.innerHTML = `<img src="" alt="" />
-                <div class="one__account__texts">
-                  <p>${element.firstName}</p>
-                  <p>Created on: <strong>${element.dateCreated}</strong></p>
-                </div>
-                <i class="ri-more-2-fill"></i>`;
+                              <div class="one__account__texts">
+                              <p>${element.firstName}</p>
+                              <p>Created on: <strong>${element.dateCreated}</strong></p>
+                              </div>
+                              <span class="account__more__div">
+  <span class="account__more__btn" style="padding: 0 3px">
+    <i class="ri-more-2-fill"></i>
+  </span>
+  <span class="account__more hide">
+    <i class="ri-delete-bin-line"></i> Delete User
+  </span>
+</span>;`;
     accountsList.appendChild(oneAccount);
   });
 }
-handleRegisteredUser();
+handleRegisteredUser(JSON.parse(localStorage.getItem("users")));
 //function to handle to delete the user
+
+const moreBtn = document.querySelectorAll(".account__more__btn");
+const accountMore = document.querySelectorAll(".account__more");
+const deleteUser = document.querySelectorAll(".account__more");
+console.log(moreBtn);
+moreBtn.forEach((oneBtn, index) => {
+  oneBtn.onclick = (e) => {
+    accountMore[index].classList.toggle("hide");
+  };
+});
+// accountMore.forEach((elem, index) => {
+//   const
+//   if(index)
+// })
+
+console.log(deleteUser);
+
+deleteUser.forEach((elem, index) => {
+  elem.addEventListener("click", () => {
+    const allAccounts = Array.from(document.querySelectorAll(".one__account"));
+    const indexToDelete = allAccounts.findIndex(
+      (elem) => Number(elem.getAttribute("data-target")) === index
+    );
+    const udpatedUsers = JSON.parse(localStorage.getItem("users")).filter(
+      (user, index) => {
+        return index !== indexToDelete;
+      }
+    );
+    localStorage.setItem("users", JSON.stringify(udpatedUsers));
+    accountsList.innerHTML = "";
+    handleRegisteredUser(udpatedUsers);
+    location.reload();
+  });
+});
